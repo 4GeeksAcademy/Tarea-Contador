36541afaeed75d199a29b875c94428bb190b7304
Antonio Gomez <totico388@hotmail.com>
2024-11-18 20:07:09 +0000

    Utilizaciondel If

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,39 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = ({digito1, digito2, digito3, digito4, digito5, digito6}) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container bg-dark text-white rounded py-3 d-flex justify-content-center align-items-center">
+			
+			<div className= "digit clock-icon p-3 text-center border border-2 border-light rounded mx-1">
+			<h1> <i class=" fa-regular fa-clock"></i> </h1>
+			</div>
+			
+			<div className= "digit clock-icon p-3 text-center border border-2 border-light rounded mx-1"> 
+			<h1>{digito1}</h1>
+			</div>
+
+            <div className= "digit clock-icon p-3 text-center border border-2 border-light rounded mx-1">
+			<h1>{digito2}</h1>
+			</div>
+
+			<div className= "digit clock-icon p-3 text-center border border-2 border-light rounded mx-1">
+			<h1>{digito3}</h1>
+			</div>
+
+			<div className= "digit clock-icon p-3 text-center border border-2 border-light rounded mx-1">
+			<h1>{digito4}</h1>
+			</div>
+
+			<div className= "digit clock-icon p-3 text-center border border-2 border-light rounded mx-1">
+			<h1>{digito5}</h1>
+			</div>
+
+			<div className= "digit clock-icon p-3 text-center border border-2 border-light rounded mx-1">
+			<h1>{digito6}</h1>
+			</div>
+
+			
 		</div>
 	);
 };
